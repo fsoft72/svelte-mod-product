3514541dff94f1920f10085b106f97da7546536b
fsoft <fabio.rotondo@gmail.com>
2024-06-09 11:56:11 +0200

    Starting Produc management components

---
+++ components/ProductEdit.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import FormCreator, { type FormField } from '$liwe3/components/FormCreator.svelte';
+	import type { Product } from '../types';
+
+	interface ProductEditProps {
+		product: Product;
+
+		// events
+		onsave: (product: Product) => void;
+	}
+
+	let { product, onsave }: ProductEditProps = $props();
+
+	const fields: FormField[] = [
+		{ name: 'code', label: 'Code', type: 'text', col: 4 },
+		{ name: 'code_forn', label: 'Code Forn', type: 'text', col: 4 },
+		{ name: 'sku', label: 'SKU', type: 'text', col: 4 },
+		{ name: 'name', label: 'Name', type: 'text', col: 4 },
+		{ name: 'short_descr', label: 'Short Description', type: 'text', col: 4 },
+		{ name: 'url', label: 'URL', type: 'text', col: 4 },
+		{ name: 'description', label: 'Description', type: 'markdown' },
+
+		{ name: 'cost', label: 'Cost', type: 'number', col: 2 },
+		{ name: 'price_net', label: 'Price NET', type: 'number', col: 2 },
+		{ name: 'price_vat', label: 'Price VAT', type: 'number', col: 2 },
+		{ name: 'curr_price_net', label: 'Curr Price NET', type: 'number', col: 2 },
+		{ name: 'curr_price_vat', label: 'Curr Price VAT', type: 'number', col: 2 },
+		{ name: 'vat', label: 'Curr Price VAT', type: 'number', col: 2 },
+
+		{ name: 'free', label: 'Free', type: 'checkbox', col: 1 },
+		{ name: 'visible', label: 'Visible', type: 'checkbox', col: 1 },
+		{ name: 'relevance', label: 'Relevance', type: 'number', col: 1 },
+		{ name: 'level', label: 'Level', type: 'number', col: 1 },
+		{ name: 'quant', label: 'Quantity', type: 'number', col: 1 },
+		{ name: 'ordered', label: 'Back ordered', type: 'number', col: 1 },
+		{ name: 'available', label: 'Available', type: 'date', col: 2 },
+		{ name: 'single', label: 'Single', type: 'checkbox', col: 4 },
+
+		{ name: 'weight', label: 'Weight (gr)', type: 'number', col: 1 },
+		{ name: 'width', label: 'Width (mm)', type: 'number', col: 1 },
+		{ name: 'height', label: 'Height (mm)', type: 'number', col: 1 },
+		{ name: 'depth', label: 'Depth (mm)', type: 'number', col: 1 },
+		{ name: 'tags', label: 'Tags', type: 'tags', col: 4 },
+		{ name: 'image', label: 'Image', type: 'media', col: 4 },
+		{ name: 'category', label: 'Category', type: 'category', col: 4 }
+	];
+
+	const onsubmit = (data: any) => {
+		onsave(data);
+	};
+</script>
+
+<div class="container">
+	<FormCreator {fields} {onsubmit} values={product} />
+</div>
+
+<style>
+</style>
--- components/ProductsAdminList.svelte
+++ components/ProductsAdminList.svelte
@@ -4,10 +4,20 @@
 		type GridDataRow,
 		type GridField
 	} from '$liwe3/components/DataGrid.svelte';
+
 	import { PencilSquare, Trash } from 'svelte-hero-icons';
-	import { product_admin_list } from '../actions';
+	import {
+		product_admin_add,
+		product_admin_details,
+		product_admin_fields,
+		product_admin_list
+	} from '../actions';
 	import { onMount } from 'svelte';
 	import type { Product } from '../types';
+	import Button from '$liwe3/components/Button.svelte';
+	import Modal from '$liwe3/components/Modal.svelte';
+	import ProductEdit from './ProductEdit.svelte';
+	import { categoryStoreList, categoriesLoad } from '$modules/category/store.svelte';
 
 	const fields: GridField[] = [
 		{ name: 'id', label: 'ID', type: 'string', hidden: true },
@@ -15,18 +25,23 @@
 		{ name: 'description', label: 'Description', type: 'string' },
 		{ name: 'price', label: 'Price', type: 'number' },
 		{ name: 'quantity', label: 'Quantity', type: 'number' },
-		{ name: 'category', label: 'Category', type: 'string' }
+		{
+			name: 'category',
+			label: 'Category',
+			type: 'string',
+			render: (value: string, row: GridDataRow) => {
+				return categoryStoreList[value]?.title ?? '';
+			}
+		}
 	];
 
-	let data: GridDataRow[] = $state([]);
-
-	let actions: GridAction[] = [
+	const actions: GridAction[] = [
 		{
 			id: 'edit',
 			label: 'Edit',
 			icon: PencilSquare,
 			action: (row: GridDataRow) => {
-				console.log('Edit', row);
+				editProduct(row);
 			}
 		},
 		{
@@ -40,12 +55,85 @@
 		}
 	];
 
-	onMount(async () => {
+	let data: GridDataRow[] = $state([]);
+	let editProductModal = $state(false);
+	let currProduct: Product | null = $state(null);
+
+	const createNewProduct = () => {
+		currProduct = {
+			id: ''
+		};
+
+		editProductModal = true;
+	};
+
+	const editProduct = async (row: GridDataRow) => {
+		const prod: Product = await product_admin_details(row.id);
+
+		currProduct = prod;
+
+		editProductModal = true;
+	};
+
+	const onsave = async (data: Record<string, any>) => {
+		console.log('Save', data);
+
+		if (!currProduct) return;
+
+		Object.assign(currProduct, data);
+
+		let res: any;
+
+		if (currProduct.id === '') {
+			res = await product_admin_add(
+				currProduct.name ?? '',
+				currProduct.code,
+				currProduct.id_maker,
+				currProduct.id_category,
+				currProduct.id_availability,
+				currProduct.code_forn,
+				currProduct.sku,
+				currProduct.description,
+				currProduct.short_description,
+				currProduct.url,
+				currProduct.cost,
+				currProduct.price_net,
+				currProduct.price_vat,
+				currProduct.curr_price_net,
+				currProduct.curr_price_vat,
+				currProduct.vat,
+				currProduct.free,
+				currProduct.discount,
+				currProduct.quant,
+				currProduct.ordered,
+				currProduct.available,
+				currProduct.level,
+				currProduct.visible,
+				currProduct.relevance,
+				currProduct.status,
+				currProduct.weight,
+				currProduct.width,
+				currProduct.height,
+				currProduct.depth,
+				currProduct.tags,
+				currProduct.single
+			);
+		} else {
+			res = await product_admin_fields(currProduct.id, currProduct);
+		}
+
+		await updateProducts();
+		editProductModal = false;
+	};
+
+	const updateProducts = async () => {
 		const prods: Product[] | any = await product_admin_list();
 
 		if (prods.error) return;
 
 		if (!prods) return;
+
+		data = [];
 
 		prods.forEach((prod: Product) => {
 			data.push({
@@ -57,18 +145,42 @@
 				category: prod.id_category
 			});
 		});
+	};
+
+	onMount(async () => {
+		await categoriesLoad();
+		await updateProducts();
 	});
 </script>
 
 <div class="container">
+	<div class="buttons">
+		<Button mode="success" onclick={createNewProduct}>New Product</Button>
+	</div>
 	<DataGrid {fields} {data} {actions} />
 </div>
+
+{#if editProductModal}
+	<Modal
+		title="Product"
+		onclose={() => (editProductModal = false)}
+		oncancel={() => (editProductModal = false)}
+	>
+		<ProductEdit product={currProduct!} {onsave} />
+	</Modal>
+{/if}
 
 <style>
 	.container {
 		display: flex;
+		flex-direction: column;
 		width: 100%;
-		flex-wrap: wrap;
 		justify-content: center;
+	}
+
+	.buttons {
+		display: flex;
+		justify-content: flex-end;
+		margin-bottom: 0.3rem;
 	}
 </style>
